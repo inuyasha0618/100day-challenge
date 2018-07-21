e82d48f32841cbd769cc81501cb16e8cf94a66af
inuyasha0618 <chenjiandiwang@qq.com>
2018-07-21 17:40:43 +0800

    day5 done

---
+++ day5/js/drawCreator.js
@@ -0,0 +1,39 @@
+import { cos, sin, pi } from './utils/math.js';
+import Matrix4 from './utils/matrix.js'
+export default ({gl, program, initialAngle=0, axisX=0, axisY=0, axisZ=1, speed=36, n=1}) => {
+    let firstTime = true;
+    let time = +new Date;
+    let angle = initialAngle;
+    const mat = gl.getUniformLocation(program, 'matrix');
+    const tcw = gl.getUniformLocation(program, 'Tcw');
+    const perspective = gl.getUniformLocation(program, 'perspective');
+    const modelMatrix = new Matrix4();
+    const tcwMat = new Matrix4().setLookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+    const perspectiveMat = new Matrix4().setPerspective(60, 1, 1.0, 100.0);
+
+    gl.uniformMatrix4fv(tcw, false, tcwMat.elements);
+    gl.uniformMatrix4fv(perspective, false, perspectiveMat.elements);
+
+    const draw = () => {
+        let now = +new Date;
+        let nowAngle = 0;
+        if (firstTime) {
+            nowAngle = initialAngle;
+            firstTime = false;
+        } else {
+            nowAngle = angle + ((now - time) / 1000 * speed) % 360;
+
+        }
+        modelMatrix.setRotate(nowAngle, axisX, axisY, axisZ);
+
+        gl.uniformMatrix4fv(mat, false, modelMatrix.elements);
+        
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+        angle = nowAngle;
+        time = now;
+        requestAnimationFrame(draw);
+    };
+
+    return draw;
+};
---
+++ day5/js/index.js
@@ -0,0 +1,68 @@
+import initGl from './utils/initGl.js';
+import drawCreator from './drawCreator.js';
+
+const cvs = document.querySelector('#cvs');
+
+const { gl, program } = initGl(cvs);
+
+// Create a cube
+//    v6----- v5
+//   /|      /|
+//  v1------v0|
+//  | |     | |
+//  | |v7---|-|v4
+//  |/      |/
+//  v2------v3
+var verticesColors = new Float32Array([
+// Vertex coordinates and color
+    1.0,  1.0,  1.0,     1.0,  1.0,  1.0,  // v0 White
+    -1.0,  1.0,  1.0,     1.0,  0.0,  1.0,  // v1 Magenta
+    -1.0, -1.0,  1.0,     1.0,  0.0,  0.0,  // v2 Red
+    1.0, -1.0,  1.0,     1.0,  1.0,  0.0,  // v3 Yellow
+    1.0, -1.0, -10.0,     0.0,  1.0,  0.0,  // v4 Green
+    1.0,  1.0, -10.0,     0.0,  1.0,  1.0,  // v5 Cyan
+    -1.0,  1.0, -10.0,     0.0,  0.0,  1.0,  // v6 Blue
+    -1.0, -1.0, -10.0,     0.0,  0.0,  0.0   // v7 Black
+]);
+
+// Indices of the vertices
+var indices = new Uint8Array([
+    0, 1, 2,   3, 2, 0,    // front
+    0, 3, 4,   0, 4, 5,    // right
+    0, 5, 6,   0, 6, 1,    // up
+    1, 6, 7,   1, 7, 2,    // left
+    7, 4, 3,   7, 3, 2,    // down
+    4, 7, 6,   4, 6, 5     // back
+]);
+
+gl.enable(gl.DEPTH_TEST);
+
+const SIZE = verticesColors.BYTES_PER_ELEMENT;
+
+let vertexColorBuffer = gl.createBuffer();
+let indexBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+
+gl.vertexAttribPointer(0, 3, gl.FLOAT, false, SIZE * 6, 0);
+gl.vertexAttribPointer(1, 3, gl.FLOAT, false, SIZE * 6, SIZE * 3);
+
+gl.enableVertexAttribArray(0);
+gl.enableVertexAttribArray(1);
+
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+const draw = drawCreator({
+    gl,
+    program,
+    initialAngle: 0,
+    axisX: 0,
+    axisY: 0,
+    axisZ: 1,
+    speed: 66,
+    n: 36
+});
+
+draw();
